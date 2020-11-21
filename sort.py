857f7198f6f2f2a2b8d5fc0a4d88d0afcdef6107
Minghua Jiang <mijian@microsoft.com>
2020-11-21 13:22:38 -0800

    update exercise

---
+++ sort.py
@@ -0,0 +1,22 @@
+def bubble_sort(list):
+    pass
+
+
+def selection_sort(list):
+    pass
+
+
+def insertion_sort(list):
+    pass
+
+
+def quick_sort(list):
+    pass
+
+
+def merge_sort(list):
+    pass
+
+
+def heap_sort(list):
+    pass
---
+++ trie.py
@@ -0,0 +1,16 @@
+class TrieNode:
+    def ___init__(self):
+        self.char_map = {}
+        self.is_leaf = False
+
+
+class Trie:
+    def insert(self, word):
+        pass
+
+    def contains(self, word):
+        pass
+
+    def start_with(self, word):
+        pass
+
